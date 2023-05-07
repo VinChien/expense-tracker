aeb8b36e334dd731d2d29efa1d9015a0b130f9e7
vinchiengithub <jianfan0114@gmail.com>
2023-05-07 17:25:38 +0800

    feat: add routes's index&home

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Express is listening on http://localhost:${PORT}`);
